c780d93260dcd7e806907c254c7898b095f22d9d
Brian Arsenault <barsenault@fsu.edu>
2020-08-19 13:09:13 -0400

    adds user id to person node created for new account

--- src/Plugin/WebformHandler/NewAccountStepThreeWebformHandler.php
+++ src/Plugin/WebformHandler/NewAccountStepThreeWebformHandler.php
@@ -35,13 +35,13 @@ class NewAccountStepThreeWebformHandler extends WebformHandlerBase {
   public function submitForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
     // Get the submitted form values
     $submission_array = $webform_submission->getData();
-    
+
     // Retrieve the data from step two from session temporary storage
     // NOTE: You have to do this before login the newly created user below,
     // as that function creates a new session for the logged in user
     $tempstore = \Drupal::service('tempstore.private')->get('ldbase_new_account');
     $match_nids = $tempstore->get('match_nids');
-    
+
     // Create Drupal User
     $user = User::create();
 
@@ -51,17 +51,17 @@ class NewAccountStepThreeWebformHandler extends WebformHandlerBase {
     $user->setEmail($submission_array['primary_email']);
     $user->setUsername($submission_array['primary_email']);
     $user->activate();
-    
+
     // Save the user
     $user->save();
-    
+
     // Create the person
     $person_title = $submission_array['preferred_display_name'];
     $person_first_name = $submission_array['ldbase_primary_name'][0]['primary_first_name'];
     $person_middle_name = $submission_array['ldbase_primary_name'][0]['primary_middle_name'];
     $person_last_name = $submission_array['ldbase_primary_name'][0]['primary_last_name'];
     $person_email = $submission_array['primary_email'];
-    
+
     $person_node = Node::create([
       'type' => 'person',
       'status' => TRUE, // published
@@ -71,8 +71,9 @@ class NewAccountStepThreeWebformHandler extends WebformHandlerBase {
       'field_last_name' => $person_last_name,
       'field_email' => $person_email,
       'field_drupal_account_id' => $user->id(),
+      'uid' => $user->id(), // set author to be this user
     ]);
-    
+
     // Save the person
     $person_node->save();
 
@@ -81,10 +82,10 @@ class NewAccountStepThreeWebformHandler extends WebformHandlerBase {
       $helper_service = \Drupal::service('ldbase_new_account_service.helper');
       $helper_service->storeExistingRecordsRequest($match_nids, $person_node->id());
     }
-    
+
     //log the user in
     user_login_finalize($user);
-    
+
     // Set the message to display in the next page
     $form_state->set('redirect_message', 'Your account has been successfully created.');
     $form_state->set('user_redirect', $user->id());
