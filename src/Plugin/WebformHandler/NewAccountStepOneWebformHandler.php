6edde2a324aebe3230e7a66568f447ac7ef1c039
Brian Arsenault <barsenault@fsu.edu>
2021-04-19 14:00:06 -0400

    issue #60 if email exists redirect back to form, not login screen

--- src/Plugin/WebformHandler/NewAccountStepOneWebformHandler.php
+++ src/Plugin/WebformHandler/NewAccountStepOneWebformHandler.php
@@ -36,6 +36,68 @@ class NewAccountStepOneWebformHandler extends WebformHandlerBase {
 
     // Get the submitted form values
     $submission_array = $webform_submission->getData();
+
+    // Create Drupal User
+    $user = User::create();
+
+    //Mandatory settings
+    $user->setPassword($submission_array['ldbase_password']);
+    $user->enforceIsNew();
+    $user->setEmail($submission_array['primary_email']);
+    $user->setUsername($submission_array['primary_email']);
+    $user->activate();
+
+    // Save the user
+    $user->save();
+
+    // Create the person
+    $person_title = $submission_array['preferred_display_name'];
+    $person_first_name = $submission_array['ldbase_primary_name'][0]['primary_first_name'];
+    $person_middle_name = $submission_array['ldbase_primary_name'][0]['primary_middle_name'];
+    $person_last_name = $submission_array['ldbase_primary_name'][0]['primary_last_name'];
+    $person_email = $submission_array['primary_email'];
+    $additional_names = $submission_array['ldbase_additional_names'];
+
+    $field_accepted_terms_of_use = date('Y-m-d\TH:i:s', time());
+
+    $person_node = Node::create([
+      'type' => 'person',
+      'status' => TRUE, // published
+      'title' => $person_title,
+      'field_first_name' => $person_first_name,
+      'field_middle_name' => $person_middle_name,
+      'field_last_name' => $person_last_name,
+      'field_email' => $person_email,
+      'field_drupal_account_id' => $user->id(),
+      'field_accepted_terms_of_use' => $field_accepted_terms_of_use,
+      'uid' => $user->id(), // set author to be this user
+    ]);
+
+    foreach($additional_names as $additional_name) {
+      $name_to_add = $additional_name['additional_first_name'] . ' ' . $additional_name['additional_middle_name']
+              . ' ' . $additional_name['additional_last_name'];
+      $person_node->field_publishing_names->appendItem($name_to_add);
+    }
+
+    // Save the person
+    $person_node->save();
+
+    //log the user in
+    user_login_finalize($user);
+
+    // Set the message and route for next page
+    $form_state->set('redirect_message', 'Your account has been successfully created.');
+    $form_state->set('message_type', 'normal');
+    $form_state->set('redirect', 'entity.user.canonical');
+    $form_state->set('user_redirect', $user->id());
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
+    $submission_array = $webform_submission->getData();
     $email_address = $submission_array['primary_email'];
 
     // Check to see if the email address has already been used
@@ -43,69 +105,10 @@ class NewAccountStepOneWebformHandler extends WebformHandlerBase {
            ->condition('name', $email_address)
            ->range(0, 1)
            ->execute();
-
-    // If the email address has already been used, send them to password reset screen
+    // if email has already been used, set error message on email field
     if(!empty($ids)){
-      $form_state->set('redirect', 'user.page');
-      $form_state->set('redirect_message', 'The email address you entered already exists.');
-      $form_state->set('message_type', 'error');
-      $form_state->set('user_redirect', '');
-    }
-    else {// If it hasn't been used, create the account
-
-      // Create Drupal User
-      $user = User::create();
-
-      //Mandatory settings
-      $user->setPassword($submission_array['ldbase_password']);
-      $user->enforceIsNew();
-      $user->setEmail($submission_array['primary_email']);
-      $user->setUsername($submission_array['primary_email']);
-      $user->activate();
-
-      // Save the user
-      $user->save();
-
-      // Create the person
-      $person_title = $submission_array['preferred_display_name'];
-      $person_first_name = $submission_array['ldbase_primary_name'][0]['primary_first_name'];
-      $person_middle_name = $submission_array['ldbase_primary_name'][0]['primary_middle_name'];
-      $person_last_name = $submission_array['ldbase_primary_name'][0]['primary_last_name'];
-      $person_email = $submission_array['primary_email'];
-      $additional_names = $submission_array['ldbase_additional_names'];
-
-      $field_accepted_terms_of_use = date('Y-m-d\TH:i:s', time());
-
-      $person_node = Node::create([
-        'type' => 'person',
-        'status' => TRUE, // published
-        'title' => $person_title,
-        'field_first_name' => $person_first_name,
-        'field_middle_name' => $person_middle_name,
-        'field_last_name' => $person_last_name,
-        'field_email' => $person_email,
-        'field_drupal_account_id' => $user->id(),
-        'field_accepted_terms_of_use' => $field_accepted_terms_of_use,
-        'uid' => $user->id(), // set author to be this user
-      ]);
-
-      foreach($additional_names as $additional_name) {
-        $name_to_add = $additional_name['additional_first_name'] . ' ' . $additional_name['additional_middle_name']
-                . ' ' . $additional_name['additional_last_name'];
-        $person_node->field_publishing_names->appendItem($name_to_add);
-      }
-
-      // Save the person
-      $person_node->save();
-
-      //log the user in
-      user_login_finalize($user);
-
-      // Set the message and route for next page
-      $form_state->set('redirect_message', 'Your account has been successfully created.');
-      $form_state->set('message_type', 'normal');
-      $form_state->set('redirect', 'entity.user.canonical');
-      $form_state->set('user_redirect', $user->id());
+      $message = 'The email address you entered already exists.';
+      $form_state->setErrorByName('primary_email', $message);
     }
   }
 
